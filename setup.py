59dcde25e0aaa8fbcb42c5a1f71bf7fec41f764a
Miguel Molero <miguel.molero@gmail.com>
2015-09-16 16:30:08 +0200

    Add setup script (very very preliminary)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pcloudpy',
+      version='0.10',
+      description='Point Cloud Viewer and Processing Toolkit in Python',
+      author='Miguel Molero-Armenta',
+      author_email='miguel.molero@gmail.com',
+      url='https://github.com/mmolero/pcloudpy',
+      packages=find_packages(),
+)
